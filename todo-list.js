91e32c7105713a46952ad80aea064b870068fc29
“Elyse <elysemyers1@gmail.com>
2019-07-25 21:08:49 -0600

    Adds properties and stubs methods for todo list class

---
+++ todo-list.js
@@ -0,0 +1,28 @@
+class ToDoList {
+  constructor() {
+    this.id = id;
+    this.title = title;
+    this.urgent = false;
+    this.tasks = [];
+  }
+
+  saveToStorage() {
+
+
+  }
+
+  deleteFromStorage() {
+
+
+  }
+
+  updateToDo() {
+
+
+  }
+
+  updateTask() {
+
+
+  }
+}
